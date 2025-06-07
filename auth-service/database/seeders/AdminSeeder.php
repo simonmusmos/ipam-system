c6a8660ef6c373e84464d06f6baa0ddeaf25670f
Simon Musmos <simonmusmos@gmail.com>
2025-06-08 02:26:38 +0800

    fix: added admin seeder

---
+++ auth-service/database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::where('name', 'superadmin')->first();
+
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $adminRole->id
+        ]);
+    }
+}
--- ui/src/plugins/axios.ts
+++ ui/src/plugins/axios.ts
@@ -3,76 +3,72 @@ import router from '../router';
 import { isLoading } from '../stores/loading';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
-  });
-  
-  // Show loader before request
-  api.interceptors.request.use(config => {
+  baseURL: 'http://localhost:8000/api',
+});
+
+// Show loader before request
+api.interceptors.request.use(config => {
+  isLoading.value = true;
+
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// Hide loader after response
+api.interceptors.response.use(
+  response => {
+    isLoading.value = false;
+    return response;
+  },
+  error => {
+    isLoading.value = false;
+
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      router.push('/login');
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+let isRefreshing = false;
+api.interceptors.request.use(async (config) => {
+  if (!isRefreshing) {
     isLoading.value = true;
-  
+    isRefreshing = true;
+    try {
+      const token = localStorage.getItem('token');
+      if (token) {
+        const response = await axios.get('http://localhost:8000/api/auth/refresh', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log(response.data);
+        const newToken = response.data.new_token;
+        if (newToken) {
+          localStorage.setItem('token', newToken);
+          config.headers.Authorization = `Bearer ${newToken}`;
+        }
+      }
+    } catch (error) {
+      console.error('Token refresh failed:', error);
+      localStorage.removeItem('token');
+    } finally {
+      isRefreshing = false;
+      isLoading.value = false;
+    }
+  } else {
     const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-  
-    return config;
-  });
-  
-  // Hide loader after response
-  api.interceptors.response.use(
-    response => {
-      isLoading.value = false;
-      return response;
-    },
-    error => {
-      isLoading.value = false;
-  
-      if (error.response?.status === 401) {
-        localStorage.removeItem('token');
-        router.push('/login');
-      }
-  
-      return Promise.reject(error);
-    }
-  );
-  
-  let isRefreshing = false;
-
-  api.interceptors.request.use(async (config) => {
-    if (!isRefreshing) {
-      isRefreshing = true;
-
-      try {
-        const token = localStorage.getItem('token');
-        if (token) {
-          const response = await axios.get('http://localhost:8000/api/auth/refresh', {
-            headers: { Authorization: `Bearer ${token}` },
-          });
-
-          console.log(response.data);
-          const newToken = response.data.new_token;
-
-          if (newToken) {
-            localStorage.setItem('token', newToken);
-            config.headers.Authorization = `Bearer ${newToken}`;
-          }
-        }
-      } catch (error) {
-        console.error('Token refresh failed:', error);
-        localStorage.removeItem('token');
-      } finally {
-        isRefreshing = false;
-      }
-    } else {
-      const token = localStorage.getItem('token');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-    }
-
-    return config;
-  }, (error) => {
-    return Promise.reject(error);
-  });
-
-  export default api;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+export default api;
